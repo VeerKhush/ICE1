c8b52cf3f5e8d4ce4f08f9893c1cf036f67274c6
khush <khush@10.48.8.23>
2024-05-22 20:16:42 -0400

    Branch Created

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,6 +4,8 @@
  */
 package card;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,12 +22,40 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random() * 13) + 1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner in = new Scanner(System.in);
+        
+        Card userCard = new Card();
+        userCard.setValue(in.nextInt());
+        userCard.setSuit(in.nextLine());
+        
         // and search magicHand here
+        boolean found = false;
+        for (Card n: magicHand)
+        {
+            if (userCard.getValue() == n.getValue() && userCard.getSuit().equals(n.getSuit()))
+            {
+                found = true;
+                break;
+            }
+        }
+       
         //Then report the result here
+        if (found)
+        {
+            System.out.println("Your card was found in magicHand");
+        }
+        else
+        {
+            System.out.println("Your card was not found in magicHand");
+        }
         // add one luckcard hard code 2,clubs
     }
     
